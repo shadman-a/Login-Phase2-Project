89babc369034405287546c3f6487f662ba39e27a
shadman-a <ahmedshadman966@gmail.com>
2021-01-28 20:48:18 -0500

    Initial commit

---
+++ src/main/java/com/hcl/loginphase2project/LoginPhase2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.loginphase2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPhase2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginPhase2ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcl/loginphase2project/LoginPhase2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.loginphase2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPhase2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
